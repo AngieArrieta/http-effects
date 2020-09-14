037750f43054c2083e26c8f8ee1c9cf77a809458
Angie Arrieta Mendoza <angiearrieta728@gmail.com>
2020-09-13 21:20:12 -0500

    firts commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
+// rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// modulos
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
+// componentes
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +20,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
